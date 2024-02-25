a5872c20c6d8774820246f9471a15993b89189a6
mohan56 <polamarasettymohan56@gmail.com>
2024-02-25 16:41:43 +0530

    modified for vite deployment:

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
+
+import VitePluginGhPages from 'vite-plugin-gh-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  
+    VitePWA(),
+   
+    VitePluginGhPages({
+      branch: 'gh-pages', // Optional: Specify the branch to deploy to
+      dir: 'dist', // Optional: Specify the build output directory
+    }),],
 })
