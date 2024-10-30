ee44acec6ba597f0011d8c1173e6c2d19b491192
mohan-56 <127687200+mohan-56@users.noreply.github.com>
2024-10-30 13:26:55 +0530

    Update

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -34,7 +34,7 @@ export default function ToDo(){
 
     return (
         <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
-      <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase'>Personal TODO APP</h2>
+      <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase'>Task Master</h2>
       <div className="flex items-center mb-4">
         <input
           id="addTodoInput"
